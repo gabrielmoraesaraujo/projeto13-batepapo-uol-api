1b9676c97b364a10c7dc88c688d70b3c91fd6957
Gabriel Moraes <gmoraes.mat@gmail.com>
2022-11-14 03:40:51 -0300

    Validação de dados

--- index.js
+++ index.js
@@ -4,3 +4,32 @@ import dotenv from 'dotenv';
 import joi from 'joi';
 import cors from 'cors';
 import dayjs from 'dayjs';
+
+dotenv.config();  
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect(() => {
+  db = mongoClient.db("db_uol");
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const participantSchema = joi.object({
+    name: joi.string().required()
+})
+
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required().pattern(/message/, /private_message/),
+    from: joi.string().required()
+})
+
+
+
+app.listen(5000, () => {
+    console.log('Server is litening on port 5000.');
+  });
